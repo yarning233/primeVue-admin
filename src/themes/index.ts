f90fe7ff86077cc997826727b79a07d9f0ea207b
yarning233 <kerui_wu@163.com>
2023-03-07 17:27:40 +0800

    feat: 使用替换css方式实现简易换肤

---
+++ src/themes/index.ts
@@ -0,0 +1,12 @@
+export const Themes = [
+  {
+    url: '/src/themes/arya-orange.css',
+    className: 'p-button-secondary',
+    label:'Secondary'
+  },
+  {
+    url: '/src/themes/arya-blue.css',
+    className: 'p-button-secondary',
+    label:'Secondary'
+  }
+]
